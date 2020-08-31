44df3c332bef805d7435366e26d2625d123d38f7
Jeong-Uk Park <jeongukpark@doai.ai>
2020-08-31 09:09:07 +0900

    Code beautification

--- src/test/kotlin/com/example/blog/HttpControllersTests.kt
+++ src/test/kotlin/com/example/blog/HttpControllersTests.kt
@@ -12,38 +12,41 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest
-class HttpControllersTests(@Autowired val mockMvc: MockMvc) {
+class HttpControllersTests(
+        @Autowired
+        val mockMvc: MockMvc
+) {
 
-	@MockkBean
-	lateinit var userRepository: UserRepository
+    @MockkBean
+    lateinit var userRepository: UserRepository
 
-	@MockkBean
-	lateinit var articleRepository: ArticleRepository
+    @MockkBean
+    lateinit var articleRepository: ArticleRepository
 
-	@Test
-	fun `List articles`() {
-		val juergen = User("springjuergen", "Juergen", "Hoeller")
-		val spring5Article = Article("Spring Framework 5.0 goes GA", "Dear Spring community ...", "Lorem ipsum", juergen)
-		val spring43Article = Article("Spring Framework 4.3 goes GA", "Dear Spring community ...", "Lorem ipsum", juergen)
-		every { articleRepository.findAllByOrderByAddedAtDesc() } returns listOf(spring5Article, spring43Article)
-		mockMvc.perform(get("/api/article/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk)
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("\$.[0].author.login").value(juergen.login))
-				.andExpect(jsonPath("\$.[0].slug").value(spring5Article.slug))
-				.andExpect(jsonPath("\$.[1].author.login").value(juergen.login))
-				.andExpect(jsonPath("\$.[1].slug").value(spring43Article.slug))
-	}
+    @Test
+    fun `List articles`() {
+        val juergen = User("springjuergen", "Juergen", "Hoeller")
+        val spring5Article = Article("Spring Framework 5.0 goes GA", "Dear Spring community ...", "Lorem ipsum", juergen)
+        val spring43Article = Article("Spring Framework 4.3 goes GA", "Dear Spring community ...", "Lorem ipsum", juergen)
+        every { articleRepository.findAllByOrderByAddedAtDesc() } returns listOf(spring5Article, spring43Article)
+        mockMvc.perform(get("/api/article/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("\$.[0].author.login").value(juergen.login))
+                .andExpect(jsonPath("\$.[0].slug").value(spring5Article.slug))
+                .andExpect(jsonPath("\$.[1].author.login").value(juergen.login))
+                .andExpect(jsonPath("\$.[1].slug").value(spring43Article.slug))
+    }
 
-	@Test
-	fun `List users`() {
-		val juergen = User("springjuergen", "Juergen", "Hoeller")
-		val smaldini = User("smaldini", "Stéphane", "Maldini")
-		every { userRepository.findAll() } returns listOf(juergen, smaldini)
-		mockMvc.perform(get("/api/user/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk)
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("\$.[0].login").value(juergen.login))
-				.andExpect(jsonPath("\$.[1].login").value(smaldini.login))
-	}
+    @Test
+    fun `List users`() {
+        val juergen = User("springjuergen", "Juergen", "Hoeller")
+        val smaldini = User("smaldini", "Stéphane", "Maldini")
+        every { userRepository.findAll() } returns listOf(juergen, smaldini)
+        mockMvc.perform(get("/api/user/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("\$.[0].login").value(juergen.login))
+                .andExpect(jsonPath("\$.[1].login").value(smaldini.login))
+    }
 }
